da0bd6af5cfc7ee5dcfda2c746f377ff1645ae5a
Jury <jury.harevich@gmail.com>
2023-09-28 17:04:22 +0300

    if clause

--- FirstRepository/src/Main.java
+++ FirstRepository/src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello mir");
-        System.out.println("pizdicha");
+        System.out.println("pizdichagit ");
+        if(true)
+            System.out.println("One more");
     }
 }
